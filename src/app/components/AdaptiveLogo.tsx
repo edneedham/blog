68a5f754f5a8e523feb5775d96728e9804df8493
Edward Needham <edwardneedham123@gmail.com>
2026-02-27 17:55:54 -0300

    chore: Fix multiple imports

--- src/app/components/AdaptiveLogo.tsx
+++ src/app/components/AdaptiveLogo.tsx
@@ -5,8 +5,6 @@
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
-import { useTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
 
 interface AdaptiveLogoProps {
   lightSrc: string
